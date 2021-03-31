18201d13386404de6311ed8fc1e444801a0006c8
Schulte, Ryan <ryan.schulte@cgi.com>
2021-03-31 16:30:29 -0700

    Add post for cars

--- api/cars_api.py
+++ api/cars_api.py
@@ -22,6 +22,12 @@ def get_db():
 def get_cars(db: SessionLocal, skip: int = 0, limit: int = 100):
     return db.query(model_car).offset(skip).limit(limit).all()
 
+def create_car(db: SessionLocal, item: schemas.ItemCreate):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
 
 @router.get("/cars/", response_model=List[model_car])
 def read_cars(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
