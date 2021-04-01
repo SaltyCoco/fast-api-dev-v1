ec6c14188849ee5b91645a994e02e686c0127e86
Schulte, Ryan <ryan.schulte@cgi.com>
2021-04-01 11:02:55 -0700

    add multiple get requests

--- api/cars_api.py
+++ api/cars_api.py
@@ -1,7 +1,7 @@
 # api/cars_api.py
 import fastapi
 from typing import List
-from fastapi import Depends
+from fastapi import Depends, status
 from sqlalchemy.orm import Session
 from models import cars_models
 from schema import cars_schema
@@ -18,8 +18,8 @@ def get_db():
         db.close()
 
 
-@router.post('/cars')
-def create_car(request: cars_schema.Car, db: Session = Depends(get_db)):
+@router.post('/cars', status_code=status.HTTP_201_CREATED)
+async def create_car(request: cars_schema.Car, db: Session = Depends(get_db)):
     new_car = cars_models.Car(
         make=request.make,
         model=request.model,
@@ -36,6 +36,30 @@ def create_car(request: cars_schema.Car, db: Session = Depends(get_db)):
 
 
 @router.get('/cars')
-def get_all_cars(db: Session = Depends(get_db)):
+async def get_all_cars(db: Session = Depends(get_db)):
     cars = db.query(cars_models.Car).all()
     return cars
+
+
+@router.get('/cars/{id}')
+async def get_car_by_id(id, db: Session = Depends(get_db)):
+    car = db.query(cars_models.Car).filter(cars_models.Car.id == id).first()
+    return car
+
+
+@router.get('/cars/model/{model}')
+async def get_car_by_model(model, db: Session = Depends(get_db)):
+    car = db.query(cars_models.Car).filter(cars_models.Car.model == model).all()
+    return car
+
+
+@router.get('/cars/make/{make}')
+async def get_car_by_make(make, db: Session = Depends(get_db)):
+    car = db.query(cars_models.Car).filter(cars_models.Car.make == make).all()
+    return car
+
+
+@router.get('/cars/yom/{year_of_manufacture}')
+async def get_car_by_year_of_manufacture(year_of_manufacture, db: Session = Depends(get_db)):
+    car = db.query(cars_models.Car).filter(cars_models.Car.year_of_manufacture == year_of_manufacture).all()
+    return car
