67da7f933e4466dd844837a24a28080d3a412da5
Ryan Schulte <ryan.j.schulte@gmail.com>
2021-04-04 11:50:16 -0700

    Azure DB with envs

--- databases/database.py
+++ databases/database.py
@@ -8,14 +8,6 @@ import urllib
 
 # DATABASE_URL = "sqlite:///./db_fastapi_dev.db"
 # DATABASE_URL = "postgres://postgres:postgres@localhost:5432/postgres"
-# params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
-#                                  "SERVER=fastapi-dev.database.windows.net;"
-#                                  "DATABASE=db_fastapi_dev;"
-#                                  "UID=RyanDevAdmin;"
-#                                  "PWD=" + SQLSERVER_PWD)
-#
-# engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
-# connect_args={"check_same_thread": False} is needed for SQLite ONLY
 # engine = create_engine(
 #     DATABASE_URL, connect_args={"check_same_thread": False}
 # )
@@ -24,14 +16,11 @@ import urllib
 # engine = create_engine(
 #     SQLSERVER_DATABASE
 # )
-# user = os.getenv('user')
-user = "RyanAdminDev"
+user = os.getenv('user')
 password = os.getenv('password')
-# host = os.getenv('SERVER_ADDRESS')
-host = "fastapi-dev.database.windows.net"
-# db = os.getenv('DATABASE')
-db = "db_fastapi_dev"
-engine = create_engine(f'mssql+pyodbc://{user}:{password}@fastapi-dev.database.windows.net/{db}?driver=ODBC+Driver+17+for+SQL+Server')
+host = os.getenv('SERVER_ADDRESS')
+db = os.getenv('DATABASE')
+engine = create_engine(f'mssql+pyodbc://{user}:{password}@{host}/{db}?driver=ODBC+Driver+17+for+SQL+Server')
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
