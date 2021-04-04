76e67f958e053ea144cdd0e8a3822d0633e6a609
Ryan Schulte <ryan.j.schulte@gmail.com>
2021-04-03 18:53:13 -0700

    azure db

--- databases/database.py
+++ databases/database.py
@@ -6,7 +6,6 @@ import pyodbc
 
 # DATABASE_URL = "sqlite:///./db_fastapi_dev.db"
 # DATABASE_URL = "postgres://postgres:postgres@localhost:5432/postgres"
-SQLSERVER_DATABASE = "mssql+pyodbc://RyanDevAdmin:RSC211982!Bryan1940!@fastapi-dev.database.windows.net/db_fastapi_dev?driver=ODBC+Driver+17+for+SQL+Server"
 
 # connect_args={"check_same_thread": False} is needed for SQLite ONLY
 # engine = create_engine(
